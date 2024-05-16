452dcfffcee1e6feec62dd025ea575f681436cb0
Cihan DEMİR <cihan.demir.1262@gmail.com>
2024-05-16 18:44:33 +0300

    Add codes for home page

--- obj/Debug/net5.0/KisiselWebSitesi.AssemblyInfo.cs
+++ obj/Debug/net5.0/KisiselWebSitesi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KisiselWebSitesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98acdc962b2531e504b056d4326d28b4edc6c7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KisiselWebSitesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KisiselWebSitesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
